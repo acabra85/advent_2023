c4329adf18f386295d023721de3d4c68b88cc6f6
Agustin Cabra <cabra.agustin@gmail.com>
2023-12-06 10:32:38 +0100

    prepare day 7

---
+++ src/main/java/com/advent23/Day7.java
@@ -0,0 +1,20 @@
+package com.advent23;
+
+/**
+ *
+ */
+public class Day7  extends AdventDayBase {
+    public Day7(String file) {
+        super(file);
+    }
+
+    @Override
+    public AdventResult solve() throws Throwable {
+        return AdventResult.ofStr(lines.getFirst());
+    }
+
+    @Override
+    public AdventResult solvePart2() throws Throwable {
+        return AdventResult.ofStr(lines.getFirst());
+    }
+}
